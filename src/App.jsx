0c9742138cb929fac75d5a2b005fb59d93bf32e6
Sudhiksha Varanasi <sudhikshavaranasi@gmail.com>
2026-07-09 21:43:59 +0530

    Initialize SoundGo Clone project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">SoundGo Clone </h1>
+    </div>
+  );
+}
+
+
+export default App
